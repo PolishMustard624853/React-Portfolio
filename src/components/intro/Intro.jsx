9d381c156fa44b340c868d1747d8efac5ea049df
Connor Semb <polishmustard710@gmail.com>
2022-06-03 22:44:36 -0500

    changed background-color, About me complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import './intro.css'
 import Me from '../../img/me.jpg'
 
+
 const Intro = () => {
   return (
     <div className='i'>
         <div className='i-left'>
           <div className='i-left-wrapper'>
             <h2 className='i-intro'>Welcome! My name is </h2>
-            <h1 className='i-name'>Connor Semb</h1>
+            <h1 className='i-name'>Connor <span>Semb</span></h1>
             <div className='i-title'>
               <div className='i-title-wrapper'>
                 <div className='i-title-item'>Front-End Web Developer</div>
@@ -19,7 +20,8 @@ const Intro = () => {
               </div>
             </div>
             <p className='i-desc'>
-              I am a front end Web Developer UI/UX Designer using programs such as HTML/CSS, JavaScript &amp; React.
+            “Two things are infinite: the universe and human stupidity; 
+            and I'm not sure about the universe.” ― Albert Einstein
             </p>
           </div>
         </div>
--- src/components/intro/about/About.jsx
+++ src/components/intro/about/About.jsx
@@ -1,9 +1,35 @@
+import React from 'react'
 import './about.css'
+import Neb from '../../../img/eagleNebula.jpg'
+import Bub from '../../../img/bubbleNebula.jpg'
 
 const About = () => {
   return (
+    <div className='i'>
     <div className='a'>
-        About
+        <div className='a-left'>
+            <div className='a-card bg'>
+              <img src={Bub} alt="" />
+            </div>
+            <div className='a-card'>
+              <img src={Neb} alt="" className='i-img' />
+            </div>
+        </div>
+        <div className='a-right'>
+        <h1 className='a-title'>About Me</h1>
+        <br></br>
+        <br></br>
+        <p className='a-sub'>
+        I am a front end Web Developer UI/UX Designer using programs such as HTML, CSS, JavaScript &amp; React.
+        <br></br>
+        <br></br>
+        </p>
+        <p className='a-desc'>
+        “I am enough of an artist to draw freely upon my imagination. Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.”
+        <p>― Albert Einstein</p>
+        </p>
+        </div>
+        </div>
     </div>
   )
 }
