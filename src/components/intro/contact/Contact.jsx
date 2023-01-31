e616cf89fac5248adfb59b3e469753ec7e85f03d
Connor Semb <polishmustard710@gmail.com>
2023-01-31 15:00:25 -0600

    contact.jsx/css improved icon spacing and added Katakana characters to each hyperlink, romanji color needs improvement

--- src/components/intro/contact/Contact.jsx
+++ src/components/intro/contact/Contact.jsx
@@ -2,7 +2,7 @@ import './contact.css'
 import Phone from '../../../assets/phone.png'
 import Email from '../../../assets/email.png'
 import Address from '../../../assets/planet.png'
-import Twitter from '../../../assets/bird.png'
+import Twitter from '../../../assets/twitter.png'
 import Github from '../../../assets/github.png'
 // import Me from '../../../img/me.jpg'
 
@@ -18,26 +18,31 @@ const Contact = () => {
                     <div className='c-info-item-1'>
                         <a href='tel: 3368251582' target='blank'>
                             <img src={Phone} alt='' className='c-icon'/>
+                            <h3>アンドロイド</h3>
                         </a>
                     </div>
                     <div className='c-info-item-2'>
                         <a href='https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2131027,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967' target='blank'>
                             <img src={Address} alt='' className='c-icon'/>
+                            <h3>ホーム</h3>
                         </a>
                     </div>
                     <div className='c-info-item-3'>
                         <a href='mailto: konaasenbu710@gmail.com' target='blank'>
                             <img src={Email} alt='' className='c-icon'/>
+                            <h3>イーメール</h3>
                         </a>
                     </div>
                     <div className='c-info-item-4'>
                         <a href='https://twitter.com/PolishMustard' target='blank'>
                             <img src={Twitter} alt='' className='c-icon'/>
+                            <h3>トウィター</h3>
                         </a>
                     </div>
                     <div className='c-info-item-5'>
                         <a href='https://github.com/PolishMustard624853' target='blank'>
                             <img src={Github} alt='' className='c-icon'/>
+                        <h3>ギトフブ</h3>
                         </a>
                     </div>
                 </div>
